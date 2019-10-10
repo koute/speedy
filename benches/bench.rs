bfa18ab944812d2f314941b5e2580ad290ba1e8e
Jan Bujak <j@exia.io>
2019-10-10 23:11:35 +0200

    Add an extra benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,11 +4,11 @@ extern crate test;
 extern crate byteorder;
 extern crate speedy;
 
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::borrow::Cow;
 use test::{Bencher, black_box};
 use byteorder::{ReadBytesExt, NativeEndian};
-use speedy::{Readable, Writable, Endianness};
+use speedy::{Context, Readable, Reader, Writable, Endianness};
 
 #[bench]
 fn deserialization_manual_bytes( b: &mut Bencher ) {
@@ -135,6 +135,35 @@ fn deserialization_speedy_megabyte_buffer_cow_owned( b: &mut Bencher ) {
     buffer = black_box( buffer );
     b.iter( || {
         let deserialized: Cow< [u8] > = Readable::read_from_buffer_owned( Endianness::NATIVE, &buffer ).unwrap();
+        deserialized
+    })
+}
+
+#[repr(transparent)]
+#[derive(Copy, Clone, Writable)]
+struct Byte( u8 );
+
+impl< 'a, C: Context > Readable< 'a, C > for Byte {
+    #[inline]
+    fn read_from< R: Reader< 'a, C > >( reader: &mut R ) -> io::Result< Self > {
+        Ok( Byte( reader.read_u8()? ) )
+    }
+
+    #[inline]
+    fn minimum_bytes_needed() -> usize {
+        1
+    }
+}
+
+#[bench]
+fn deserialization_speedy_megabyte_buffer_cow_non_primitive( b: &mut Bencher ) {
+    let mut buffer: Vec< Byte > = Vec::new();
+    buffer.resize( 1024 * 1024, Byte( 1 ) );
+    let mut buffer = buffer.write_to_vec( Endianness::NATIVE ).unwrap();
+
+    buffer = black_box( buffer );
+    b.iter( || {
+        let deserialized: Cow< [Byte] > = Readable::read_from_buffer_owned( Endianness::NATIVE, &buffer ).unwrap();
         deserialized
     })
 }
